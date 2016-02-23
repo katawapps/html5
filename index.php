1037cf6415f7f43609b8a882438488dbfdf42993
katawapps <katawapps@gmail.com>
2016-02-23 17:09:10 -0500

    add index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+
+<nav>
+	<ul>
+		<li><a href="">INICIO</a></li>
+		<li><a href="">NOSOTROS</a></li>
+		<li><a href="">GALERIA</a></li>
+		<li><a href="">CONTACTENOS</a></li>
+	</ul>
+	
+
+</nav>
+<center>
+<img src="1.gif" width="350" alt="">
+</center>
+<a target="_blank" href="https://www.google.com.pe">IR A GOOGLE</a>
+
+<style>
+	ul{
+		    list-style-type: none;	
+	}
+	a{
+		text-decoration: none;
+	}
+	li{
+		    display: inline-block;
+    background-color: red;
+    padding: 8px;
+    border-radius: 28px;
+	}
+	a{
+		    font-size: 20px;
+    color: white;
+    margin: 50px;
+    padding: 0px 2px;
+	}
+
+
+
+</style>
+
+</body>
+</html>
